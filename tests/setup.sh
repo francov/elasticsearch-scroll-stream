a01cba1ba27026b813ae93c8586aeb5b2755855e
Francesco Valente <francesco83.valente@gmail.com>
2018-12-05 14:22:19 +0100

    Update elastic docker image and tests setup

--- tests/setup.sh
+++ tests/setup.sh
@@ -8,21 +8,22 @@ echo -e "Setting up the test index"
 
 for i in `seq 1 20`;
 do
-  curl -s -XPUT "$BASE/elasticsearch-test-scroll-stream/test-type/$i" -d '
+  curl -s -XPUT "$BASE/elasticsearch-test-scroll-stream/test-type/$i" -H 'Content-Type: application/json' -d '
     {"name": "first chunk name", "other": "first other data"}
   '
 done
 
 for i in `seq 21 40`;
 do
-  curl -s -XPUT "$BASE/elasticsearch-test-scroll-stream/test-type/$i" -d '
+  curl -s -XPUT "$BASE/elasticsearch-test-scroll-stream/test-type/$i" -H 'Content-Type: application/json' -d '
     {"name": "second chunk name", "other": "second other data"}
   '
 done
 
 for i in `seq 41 60`;
 do
-  curl -s -XPUT "$BASE/elasticsearch-test-scroll-stream/test-type/$i" -d '
+  curl -s -XPUT "$BASE/elasticsearch-test-scroll-stream/test-type/$i" -H 'Content-Type: application/json' -d '
     {"name": "third chunk name", "other": "third other data"}
   '
 done
+
