fcb1df5b53db6efa9f7fd2960bd36aeb62b72369
Francesco Valente <francesco83.valente@gmail.com>
2019-07-19 18:33:34 +0200

    Remove support to old "elastical" client

--- index.js
+++ index.js
@@ -3,10 +3,9 @@
  *
  * Create a ReadableStream from an elasticsearch scroll query.
  */
-var LibElasticalAdaptee = require("./lib/elastical-stream"),
-    LibElasticsearchAdaptee = require("./lib/elasticsearch-stream");
+var LibElasticsearchAdaptee = require('./lib/elasticsearch-stream')
 
-var allowed_extrafields = ['_id', '_score', '_type', '_index', '_parent', '_routing', 'inner_hits'];
+var allowed_extrafields = ['_id', '_score', '_type', '_index', '_parent', '_routing', 'inner_hits']
 
 /**
  * ElasticsearchScrollStream
@@ -18,26 +17,22 @@ var allowed_extrafields = ['_id', '_score', '_type', '_index', '_parent', '_rout
  * @param `stream_opts` - object to be passed to ReadableStream
  */
 var ElasticsearchScrollStream = function(client, query_opts, optional_fields, stream_opts) {
-  if (arguments.length == 1) throw new Error("ElasticsearchScrollStream: missing parameters");
-  if (!client) throw new Error("ElasticsearchScrollStream: client is ", client);
+  if (arguments.length == 1) throw new Error('ElasticsearchScrollStream: missing parameters')
+  if (!client) throw new Error('ElasticsearchScrollStream: client is ', client)
 
-  optional_fields = (!!optional_fields) ? optional_fields : []
-  if (!Array.isArray(optional_fields)) throw new Error("ElasticsearchScrollStream: optional_fields must be an array", optional_fields);
+  optional_fields = !!optional_fields ? optional_fields : []
+  if (!Array.isArray(optional_fields))
+    throw new Error('ElasticsearchScrollStream: optional_fields must be an array', optional_fields)
 
   optional_fields.forEach(function(entry) {
     if (allowed_extrafields.indexOf(entry) == -1) {
-      throw new Error("ElasticsearchScrollStream: property '" + entry + "' not allowed in optional_fields");
+      throw new Error("ElasticsearchScrollStream: property '" + entry + "' not allowed in optional_fields")
     }
-  });
+  })
 
-  stream_opts = (!!stream_opts) ? stream_opts : {};
+  stream_opts = !!stream_opts ? stream_opts : {}
 
-  if (!!client.nodes) {
-    return new LibElasticsearchAdaptee(client, query_opts, optional_fields, stream_opts);
-  } else {
-    return new LibElasticalAdaptee(client, query_opts, stream_opts);
-  }
-};
+  return new LibElasticsearchAdaptee(client, query_opts, optional_fields, stream_opts)
+}
 
-module.exports = ElasticsearchScrollStream;
-
+module.exports = ElasticsearchScrollStream
