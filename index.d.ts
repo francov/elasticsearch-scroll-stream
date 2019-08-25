e7f13cd209585a698a82e70bb7ba6d0ff40f26e7
Thomas Bonk <thomas@bonk.dev>
2019-08-25 13:46:23 +0200

    Add typescript typings

---
+++ index.d.ts
@@ -0,0 +1,23 @@
+import { Client } from "@elastic/elasticsearch";
+import { Search } from "@elastic/elasticsearch/api/requestParams";
+import { Readable, ReadableOptions } from "stream";
+
+export = ElasticsearchScrollStream;
+type optionalField =
+  | "_id"
+  | "_score"
+  | "_type"
+  | "_index"
+  | "_parent"
+  | "_routing";
+
+declare class ElasticsearchScrollStream extends Readable {
+  public constructor(
+    client: Client,
+    clientOptions?: Search,
+    optionalFields?: optionalField[],
+    readableOptions?: ReadableOptions
+  );
+}
+
+declare namespace ElasticsearchScrollStream {}
